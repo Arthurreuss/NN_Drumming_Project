96922a1850909807760421f12ba079447fd0bde0
Arthurreuss <arthur.reuss@gmx.de>
2025-10-05 00:02:15 +0200

    renamed beat into timestep because it is more accurate

--- ml/data/dataset_generator.py
+++ ml/data/dataset_generator.py
@@ -1,26 +1,28 @@
 import os
+
 import pandas as pd
-from ml.generate_data.create_matrix import CreateMatrix
+from ml.data.create_matrix import CreateMatrix
 
 
 class DatasetGenerator:
-    def __init__(self, verbose=True, dataset_path="datasets/e-gmd-v1.0.0", dataset_config=None):
+    def __init__(
+        self, verbose=True, dataset_path="datasets/e-gmd-v1.0.0", dataset_config=None
+    ):
         if not dataset_config:
             raise ValueError("dataset_config must be provided")
-        
 
-        self.beats_per_bar = dataset_config['beats_per_bar']
-        self.quantization = dataset_config['quantization']
-        self.normalize_velocity = dataset_config['normalize_velocity']
+        self.beats_per_bar = dataset_config["beats_per_bar"]
+        self.quantization = dataset_config["quantization"]
+        self.normalize_velocity = dataset_config["normalize_velocity"]
         print(f"Dataset config: {dataset_config}")
 
         self.verbose = verbose
         if not os.path.exists(dataset_path):
             raise ValueError(f"Dataset path {dataset_path} does not exist.")
-        
+
         self.dataset_path = dataset_path
         self.data_csv = pd.read_csv(os.path.join(dataset_path, "e-gmd-v1.0.0.csv"))
-        self.midi_file_names = self.data_csv['midi_filename'].tolist()
+        self.midi_file_names = self.data_csv["midi_filename"].tolist()
 
     def _print(self, message):
         if self.verbose:
@@ -32,29 +34,39 @@ class DatasetGenerator:
             if not os.path.exists(os.path.join(self.dataset_path, file_name)):
                 self._print(f"Warning: {file_name} does not exist in dataset path.")
                 continue
-            data_point = self.data_csv[self.data_csv['midi_filename'] == file_name].iloc[0]
-            bpm = data_point['bpm']
-            beat_type = data_point['beat_type']
-            style = data_point['style']
+            data_point = self.data_csv[
+                self.data_csv["midi_filename"] == file_name
+            ].iloc[0]
+            bpm = data_point["bpm"]
+            beat_type = data_point["beat_type"]
+            style = data_point["style"]
             file_path = os.path.join(self.dataset_path, file_name)
-            matrix_gen = CreateMatrix(quantization=self.quantization, beats_per_bar=self.beats_per_bar, normalize_velocity=self.normalize_velocity)
+            matrix_gen = CreateMatrix(
+                quantization=self.quantization,
+                beats_per_bar=self.beats_per_bar,
+                normalize_velocity=self.normalize_velocity,
+            )
             matrixes = matrix_gen.run(file_path)
-            samples.append({
-                "file_name": file_name,
-                "bpm": bpm,
-                "beat_type": beat_type,
-                "style": style,
-                "matrixes": matrixes
-            })
-            print(f'Progress: {len(samples)}/{len(self.midi_file_names)}', end='\r')
+            samples.append(
+                {
+                    "file_name": file_name,
+                    "bpm": bpm,
+                    "beat_type": beat_type,
+                    "style": style,
+                    "matrixes": matrixes,
+                }
+            )
+            print(f"Progress: {len(samples)}/{len(self.midi_file_names)}", end="\r")
         self.samples = samples
 
-    def save_samples(self, save_path="datasets/preprocessed_datasets/processed_samples.csv"):
-        #check if dir exists else create it
+    def save_samples(
+        self, save_path="datasets/preprocessed_datasets/processed_samples.csv"
+    ):
+        # check if dir exists else create it
         dir_name = os.path.dirname(save_path)
         if not os.path.exists(dir_name):
             os.makedirs(dir_name)
-        if not hasattr(self, 'samples'):
+        if not hasattr(self, "samples"):
             raise ValueError("No samples to save. Please run create_samples() first.")
         df = pd.DataFrame(self.samples)
         df.to_csv(save_path, index=False)
--- ml/data/midi.py
+++ ml/data/midi.py
@@ -11,7 +11,7 @@ class Midi:
         self._quantization = quantization
         self._pitch_dim = 128  # pitch dimension
         self._total_ticks = None  # length in MIDI ticks
-        self._total_beats = None  # time steps in quantized drum matrix
+        self._total_timesteps = None  # time steps in quantized drum matrix
         self.tracks = {}  # drum matrices for each track
         self._notes_on_temp = []
         self._unnamed_tracks = 0
@@ -37,20 +37,20 @@ class Midi:
                     max_pitch = max(max_pitch, pitch)
         return min_pitch, max_pitch
 
-    def _get_total_beats(self):
+    def _get_total_timesteps(self):
         mid = MidiFile(self._song_path)
         ticks_per_beat = mid.ticks_per_beat
         self._get_total_ticks()
-        self._total_beats = int(
+        self._total_timesteps = int(
             (self._total_ticks / ticks_per_beat) * self._quantization
         )
 
     def _add_note_to_drum_matrix(self, note_off, track_name):
-        pitch_off, _, beat_off = note_off
-        for idx, (pitch_on, velocity_on, beat_on) in enumerate(self._notes_on_temp):
+        pitch_off, _, timestep_off = note_off
+        for idx, (pitch_on, velocity_on, timestep_on) in enumerate(self._notes_on_temp):
             if pitch_on == pitch_off:
                 self.tracks[track_name]["drum_matrix"][
-                    beat_on:beat_off, pitch_on
+                    timestep_on:timestep_off, pitch_on
                 ] = velocity_on
 
                 del self._notes_on_temp[idx]
@@ -60,7 +60,7 @@ class Midi:
         self._song_path = song_path
         mid = MidiFile(self._song_path)
         ticks_per_beat = mid.ticks_per_beat
-        self._get_total_beats()
+        self._get_total_timesteps()
 
         for idx_track, track in enumerate(mid.tracks):
             self._notes_on_temp = []
@@ -70,7 +70,7 @@ class Midi:
                 self._unnamed_tracks += 1
             self.tracks[name] = {
                 "drum_matrix": np.zeros(
-                    [self._total_beats, self._pitch_dim], dtype=np.int16
+                    [self._total_timesteps, self._pitch_dim], dtype=np.int16
                 ),
                 "genre": None,  # todo
             }
@@ -78,18 +78,18 @@ class Midi:
             abs_ticks = 0
             for message in track:
                 abs_ticks += message.time  # accumulate ticks
-                beat = int(abs_ticks * self._quantization / ticks_per_beat)
+                timestep = int(abs_ticks * self._quantization / ticks_per_beat)
 
                 if message.type == "note_on":
                     pitch, velocity = message.note, message.velocity
                     if velocity > 0:
-                        self._notes_on_temp.append((pitch, velocity, beat))
+                        self._notes_on_temp.append((pitch, velocity, timestep))
                     else:
-                        self._add_note_to_drum_matrix((pitch, 0, beat), name)
+                        self._add_note_to_drum_matrix((pitch, 0, timestep), name)
 
                 elif message.type == "note_off":
                     pitch, velocity = message.note, message.velocity
-                    self._add_note_to_drum_matrix((pitch, velocity, beat), name)
+                    self._add_note_to_drum_matrix((pitch, velocity, timestep), name)
             if np.count_nonzero(self.tracks[name]["drum_matrix"]) == 0:
                 del self.tracks[name]
         return self.tracks
