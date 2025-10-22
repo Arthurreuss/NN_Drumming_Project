2fb340e17b94b5eaa5facb5da90cbe00cc8395c4
Arthurreuss <arthur.reuss@gmx.de>
2025-10-22 17:13:37 +0200

    debug for habrok

--- scripts/run_job.sh
+++ scripts/run_job.sh
@@ -8,6 +8,7 @@ module --force purge
 module load Python/3.12.3-GCCcore-13.3.0
 source $HOME/NN_Drumming_Project/NNvenv/bin/activate
 
+export CUDA_LAUNCH_BLOCKING=1
 
 python -m main --model $1 --learning_rate $2
 
